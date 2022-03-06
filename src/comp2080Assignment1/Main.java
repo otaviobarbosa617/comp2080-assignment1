d3ecd69ac84cbc99c4265a4a1c1b7fe760f0fb0b
otaviobarbosa617 <barbosaoapb@gmail.com>
2022-03-06 10:54:57 -0500

    insertion and selection sort desc

--- src/comp2080Assignment1/Main.java
+++ src/comp2080Assignment1/Main.java
@@ -4,6 +4,31 @@ import java.util.*;
 
 public class Main {
 
+    public static void selectionSortDesc(int[] arrayofSize){
+        for (int i = 0; i < arrayofSize.length; i++){
+            int minimum = i;
+            for (int j = i+1; j < arrayofSize.length; j++){
+                if (arrayofSize[j] > arrayofSize[minimum]){
+                    minimum = j;
+                }
+            }
+            int temporary = arrayofSize[minimum];
+            arrayofSize[minimum] = arrayofSize[i];
+            arrayofSize[i] = temporary;
+        }
+    }
+
+    public static void InsertionSortDesc(int[] arrayofSize){
+        for (int i = 1; i < arrayofSize.length; i++){
+            int temporary = arrayofSize[i];
+            int j = i - 1;
+            while (j >= 0 && temporary > arrayofSize[j]){
+                arrayofSize[j + 1] = arrayofSize[j];
+                --j;
+            }
+            arrayofSize[j + 1] = temporary;
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -56,6 +81,61 @@ public class Main {
             dataTenThousand2[i] = coreData[i];
             dataTenThousand3[i] = coreData[i];
             dataTenThousand4[i] = coreData[i];
+        }
+
+        // 4 copies of 100000 size
+
+        int[] dataOneHundredThou1 = new int[100000];
+        int[] dataOneHundredThou2 = new int[100000];
+        int[] dataOneHundredThou3 = new int[100000];
+        int[] dataOneHundredThou4 = new int[100000];
+
+        for (int i = 0; i < 100000; i++){
+            dataOneHundredThou1[i] = coreData[i];
+            dataOneHundredThou2[i] = coreData[i];
+            dataOneHundredThou3[i] = coreData[i];
+            dataOneHundredThou4[i] = coreData[i];
+        }
+
+        // 4 copies of 1000000 size
+
+        int[] dataOneMillion1 = new int[1000000];
+        int[] dataOneMillion2 = new int[1000000];
+        int[] dataOneMillion3 = new int[1000000];
+        int[] dataOneMillion4 = new int[1000000];
+
+        for (int i = 0; i < 1000000; i++){
+            dataOneMillion1[i] = coreData[i];
+            dataOneMillion2[i] = coreData[i];
+            dataOneMillion3[i] = coreData[i];
+            dataOneMillion4[i] = coreData[i];
+        }
+
+
+
+        System.out.println("Core data:");
+        for (int i = 0; i < 50; i++){
+            System.out.println(i+1 + " - " + coreData[i]);
+        }
+        System.out.println("");
+        System.out.println("Copy 50 data:");
+        for (int i = 0; i < 50; i++){
+            System.out.println(i+1 + " - " + data50n1[i]);
+        }
+
+        selectionSortDesc(data50n1);
+        System.out.println("");
+        System.out.println("Selection sort - 50 data:");
+        for (int i = 0; i < 50; i++){
+            System.out.println(i+1 + " - " + data50n1[i]);
+        }
+
+
+        InsertionSortDesc(data50n2);
+        System.out.println("");
+        System.out.println("Insertion sort - 50 data:");
+        for (int i = 0; i < 50; i++){
+            System.out.println(i+1 + " - " + data50n2[i]);
         }
 
 
