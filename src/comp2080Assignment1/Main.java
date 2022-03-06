a19007f0f852a1861903eec105d66f78d4c340d3
otaviobarbosa617 <barbosaoapb@gmail.com>
2022-03-06 11:41:06 -0500

    merge sort desc

--- src/comp2080Assignment1/Main.java
+++ src/comp2080Assignment1/Main.java
@@ -4,31 +4,72 @@ import java.util.*;
 
 public class Main {
 
-    public static void selectionSortDesc(int[] arrayofSize){
-        for (int i = 0; i < arrayofSize.length; i++){
+    public static void selectionSortDesc(int[] arrayOfSize){
+        for (int i = 0; i < arrayOfSize.length; i++){
             int minimum = i;
-            for (int j = i+1; j < arrayofSize.length; j++){
-                if (arrayofSize[j] > arrayofSize[minimum]){
+            for (int j = i+1; j < arrayOfSize.length; j++){
+                if (arrayOfSize[j] > arrayOfSize[minimum]){
                     minimum = j;
                 }
             }
-            int temporary = arrayofSize[minimum];
-            arrayofSize[minimum] = arrayofSize[i];
-            arrayofSize[i] = temporary;
+            int temporary = arrayOfSize[minimum];
+            arrayOfSize[minimum] = arrayOfSize[i];
+            arrayOfSize[i] = temporary;
         }
     }
 
-    public static void InsertionSortDesc(int[] arrayofSize){
-        for (int i = 1; i < arrayofSize.length; i++){
-            int temporary = arrayofSize[i];
+    public static void InsertionSortDesc(int[] arrayOfSize){
+        for (int i = 1; i < arrayOfSize.length; i++){
+            int temporary = arrayOfSize[i];
             int j = i - 1;
-            while (j >= 0 && temporary > arrayofSize[j]){
-                arrayofSize[j + 1] = arrayofSize[j];
+            while (j >= 0 && temporary > arrayOfSize[j]){
+                arrayOfSize[j + 1] = arrayOfSize[j];
                 --j;
             }
-            arrayofSize[j + 1] = temporary;
+            arrayOfSize[j + 1] = temporary;
         }
     }
+
+    public static void MergeSortDesc(int[] arrayOfSize){
+        if (arrayOfSize.length < 2){
+            return;
+        }
+        int mid = arrayOfSize.length/2;
+
+        int[] tempLeft = new int[mid];
+        int[] tempRight = new int[arrayOfSize.length - mid];
+
+        System.arraycopy(arrayOfSize, 0, tempLeft, 0, mid);
+
+        for (int i = mid; i < arrayOfSize.length; i++){
+            tempRight[i - mid] = arrayOfSize[i];
+        }
+
+        MergeSortDesc(tempLeft);
+        MergeSortDesc(tempRight);
+        merge(arrayOfSize, tempLeft, tempRight);
+    }
+
+    public static void merge(int[] temporaryArray, int[] tempLeft, int [] tempRight){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < tempLeft.length && j < tempRight.length){
+            if (tempLeft[i] > tempRight[j]){
+                temporaryArray[k++] = tempLeft[i++];
+            }
+            else {
+                temporaryArray[k++] = tempRight[j++];
+            }
+        }
+        while (i < tempLeft.length){
+            temporaryArray[k++] = tempLeft[i++];
+        }
+        while (j < tempRight.length){
+            temporaryArray[k++] = tempRight[j++];
+        }
+    }
+
 
 
     public static void main(String[] args) {
@@ -130,12 +171,18 @@ public class Main {
             System.out.println(i+1 + " - " + data50n1[i]);
         }
 
-
         InsertionSortDesc(data50n2);
         System.out.println("");
         System.out.println("Insertion sort - 50 data:");
         for (int i = 0; i < 50; i++){
             System.out.println(i+1 + " - " + data50n2[i]);
+        }
+
+        MergeSortDesc(data50n3);
+        System.out.println("");
+        System.out.println("Merge sort - 50 data:");
+        for (int i = 0; i < 50; i++){
+            System.out.println(i+1 + " - " + data50n3[i]);
         }
 
 
